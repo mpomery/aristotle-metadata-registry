f158ed1ec843581697a45442e1bb85181ba9de34
Samuel Spencer <theodore.therone@gmail.com>
2016-10-31 05:42:01 +0000

    add haystack elasticsearch, because the new version has broken stuff

--- aristotle_mdr/tests/settings/settings.py
+++ aristotle_mdr/tests/settings/settings.py
@@ -49,7 +49,10 @@ if 'TRAVIS' in os.environ:
         from aristotle_mdr.tests.settings.templates.search.whoosh import HAYSTACK_CONNECTIONS
     elif os.environ.get('SEARCH') == 'elasticsearch':
         print("Running TRAVIS-CI test-suite with elasticsearch")
-        from aristotle_mdr.tests.settings.templates.search.elasticsearch import HAYSTACK_CONNECTIONS
+        if os.environ.get('SEARCH') == 'haystack':
+            from aristotle_mdr.tests.settings.templates.search.haystack_elasticsearch import HAYSTACK_CONNECTIONS
+        else:
+            from aristotle_mdr.tests.settings.templates.search.elasticsearch import HAYSTACK_CONNECTIONS
 
 if 'ARISTOTLE_DEV_SKIP_MIGRATIONS' in os.environ or os.environ.get('DB') == 'mysql':  # pragma: no cover
     print("Skipping migrations")
