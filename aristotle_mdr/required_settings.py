bc25c533f27a431fdace26161a51d2d76b588c34
Samuel Spencer <theodore.therone@gmail.com>
2017-01-23 04:33:59 +0000

    fix pep8 bug

--- aristotle_mdr/required_settings.py
+++ aristotle_mdr/required_settings.py
@@ -54,9 +54,9 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 # Required for admindocs, see: https://code.djangoproject.com/ticket/21386
 SITE_ID=None
 
-# This gets called because of teh DataElementConcept.property attribute.
+# This gets called because of the DataElementConcept.property attribute.
 # Not sure how to resolve this one yet.
-SILENCED_SYSTEM_CHECKS = ['models.E006'] 
+SILENCED_SYSTEM_CHECKS = ['models.E006']
 
 ALLOWED_HOSTS = []
 
