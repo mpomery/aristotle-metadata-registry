2cb9670057b6cfc574cbd99a6b89243db3f17871
Samuel Spencer <theodore.therone@gmail.com>
2017-02-10 00:46:18 +0000

    add autocomplete test

--- aristotle_mdr/tests/settings/settings.py
+++ aristotle_mdr/tests/settings/settings.py
@@ -9,7 +9,7 @@ sys.path.insert(1, os.path.join(BASE, "tests"))
 sys.path.insert(1, os.path.join(BASE, "tests/apps"))
 
 TEMPLATES[0]['DIRS'] = [
-    os.path.join(BASE_DIR, 'tests/apps/bulk_actions_test/templates')
+    os.path.join(BASE, 'tests/apps/bulk_actions_test/templates')
 ]
 
 SECRET_KEY = 'inara+vtkprm7@0(fsc$+grbz9-s+tmo9d)e#k(9uf8m281&$7xhdkjr'
