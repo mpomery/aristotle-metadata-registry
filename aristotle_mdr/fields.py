73841015794605ab9d7ffbbabfd0f8d270bc260a
Samuel Spencer <theodore.therone@gmail.com>
2017-06-03 01:57:04 +0000

    add help for new fields

--- aristotle_mdr/fields.py
+++ aristotle_mdr/fields.py
@@ -1,3 +1,21 @@
+"""
+Concept model relations
+-----------------------
+
+These are direct reimplementations of Django model relations,
+at the moment they onyl exist to make permissions-based filtering easier for
+the GraphQL codebase. However, in future these may add additional functionality
+such as automatically applying certain permissions to ensure users only 
+retrieve the right objects.
+
+When building models that link to any subclass of ``_concept``, use these in place
+of the Django builtins.
+
+.. note:: The model these are place on does *not* need to be a subclass of concept.
+ They are for linking *to* a concept subclass.
+
+"""
+
 from django.db.models import (
     ForeignKey, ManyToOneRel,
     ManyToManyField, ManyToManyRel,
@@ -10,6 +28,10 @@ class ConceptOneToOneRel(OneToOneRel):
 
 
 class ConceptOneToOneField(OneToOneField):
+    """
+    Reimplementation of ``OneToOneField`` for linking
+    a model to a Concept
+    """
     rel_class = ConceptOneToOneRel
 
 
@@ -18,6 +40,10 @@ class ConceptManyToOneRel(ManyToOneRel):
 
 
 class ConceptForeignKey(ForeignKey):
+    """
+    Reimplementation of ``ForeignKey`` for linking
+    a model to a Concept
+    """
     rel_class = ConceptManyToOneRel
 
 
@@ -26,4 +52,8 @@ class ConceptManyToManyRel(ManyToManyRel):
 
 
 class ConceptManyToManyField(ManyToManyField):
+    """
+    Reimplementation of ``ManyToManyField`` for linking
+    a model to a Concept
+    """
     rel_class = ConceptManyToManyRel
