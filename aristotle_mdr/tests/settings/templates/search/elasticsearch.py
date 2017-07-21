4ea61c69b2936a2c972b9676b41df3b92356019c
Samuel Spencer <theodore.therone@gmail.com>
2017-07-21 05:25:52 +0000

    travis forces elastic search5

--- aristotle_mdr/tests/settings/templates/search/elasticsearch.py
+++ aristotle_mdr/tests/settings/templates/search/elasticsearch.py
@@ -2,7 +2,7 @@ HAYSTACK_CONNECTIONS = {
     'default': {
         # 'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         # 'ENGINE': 'aristotle_mdr.contrib.search_backends.elasticsearch2.Elasticsearch2SearchEngine',
-        'ENGINE': 'haystack_elasticsearch.elasticsearch2.Elasticsearch2SearchEngine',
+        'ENGINE': 'haystack_elasticsearch.elasticsearch5.Elasticsearch5SearchEngine',
         'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
     },
