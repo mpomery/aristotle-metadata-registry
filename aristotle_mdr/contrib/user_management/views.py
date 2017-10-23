88c6b383758c62556aaefb30dcbde0c47d7d40a1
Samuel Spencer <theodore.therone@gmail.com>
2017-10-23 10:20:27 +0000

    peppy

--- aristotle_mdr/contrib/user_management/views.py
+++ aristotle_mdr/contrib/user_management/views.py
@@ -22,7 +22,9 @@ class RegistryOwnerUserList(LoginRequiredMixin, PermissionRequiredMixin, ListVie
 
     def get_queryset(self):
         q = self.request.GET.get('q', None)
-        queryset = get_user_model().objects.all().order_by('is_active','first_name','last_name','email','username')
+        queryset = get_user_model().objects.all().order_by(
+            'is_active', 'first_name', 'last_name', 'email', 'username'
+        )
         if q:
             queryset = queryset.filter(
                 Q(username__icontains=q) |
