0647aed0ef0e71d8552a8809ee9357022a366c6c
Samuel Spencer <theodore.therone@gmail.com>
2018-01-31 00:18:50 +0000

    better path checking

--- scripts/check_path.sh
+++ scripts/check_path.sh
@@ -1,4 +1,4 @@
-if git diff HEAD~ --name-only|grep $1 &>/dev/null; then
+if git diff --name-only HEAD...$TRAVIS_BRANCH|grep ^python/$1 &>/dev/null; then
     # Folder is in latest check in
     exit 0
 else
