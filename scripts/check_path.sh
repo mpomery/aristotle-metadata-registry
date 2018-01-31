300a97dd13858bb245f0f2f0c2886bfe3ff287d7
Samuel Spencer <theodore.therone@gmail.com>
2018-01-31 04:28:42 +0000

    fix path checker

--- scripts/check_path.sh
+++ scripts/check_path.sh
@@ -1,6 +1,7 @@
-if git diff --name-only HEAD...$TRAVIS_BRANCH|grep ^python/$1 &>/dev/null; then
+if git diff --name-only $TRAVIS_BRANCH...HEAD|grep ^python/$1 &>/dev/null; then
     # Folder is in latest check in
     exit 0
 else
+    echo "No changes to" $1 "on branch" $TRAVIS_BRANCH "skipping tests"
     exit 1
 fi;
