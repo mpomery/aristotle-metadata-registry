73dc458cf63ef2116adfa8b8e016f7239a8ec18a
Sam Spencer <theodore.therone@gmail.com>
2018-03-09 05:54:18 +0000

    peppy

--- python/aristotle-metadata-registry/aristotle_mdr/contrib/links/apps.py
+++ python/aristotle-metadata-registry/aristotle_mdr/contrib/links/apps.py
@@ -8,7 +8,7 @@ class Config(AppConfig):
     description = """
     This module allows users of the registry to create taxonomies using custom relationships with associated roles,
     which can be used to create links between metadata items.
-    
+
     For example, a user could create a "Broader than" relationship, with "broader term" and "narrower term" as the roles within the relationship.
     A new link could be made based on this relationship with the broader term end connecting to the Object Class "Person" and the narrower term end pointing to the Object Class "Employee".
     """
