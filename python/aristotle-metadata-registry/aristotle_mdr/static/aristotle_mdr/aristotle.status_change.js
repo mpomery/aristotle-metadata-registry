84e13b8032946e90947a6c083363b7dc4154048a
Harry White <harry.white424@gmail.com>
2018-03-20 15:19:10 +1100

    Fix for the status change javascript

--- python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/aristotle.status_change.js
+++ python/aristotle-metadata-registry/aristotle_mdr/static/aristotle_mdr/aristotle.status_change.js
@@ -9,37 +9,39 @@
 /* global diff_visibility_text */
 /* global current_visibility */
 
-$("#changeStatus input").change(function() {
-    var state = $('#changeStatus input[name=state]:checked').val();
-    var selected_ras = $("#changeStatus input[name^=registration]:checked");
-    if ((typeof state === "undefined") || (selected_ras.length == 0)){
-        $('#potential').html(select_state_to_see);   
-    } else {
-        $('#potential').html(this.name);
-        var new_visibility = "hidden";
-        selected_ras.each(function( index ) {
-            var ra = $(this).val();
-            console.log(
-                status_matrix[ra].states[state]
-            );
-            var potential_vis = status_matrix[ra].states[state];
-            if (potential_vis == "public") {
-                new_visibility = potential_vis;
-            } else if (new_visibility == "public") {
-                // Do nothing, if one thinks its public, its public
-            } else if (potential_vis == "locked") {
-                new_visibility = potential_vis;
-            }
-        });
-        var msg = "";
-        var vis_trans = visibility_translations[new_visibility];
-        if (new_visibility == current_visibility) {
-            msg = same_visibility_text;
+$(document).ready(function() {
+    $("#changeStatus input").change(function() {
+        var state = $('#changeStatus input[name$=state]:checked').val();
+        var selected_ras = $("#changeStatus input[name$=registrationAuthorities]:checked");
+        if ((typeof state === "undefined") || (selected_ras.length == 0)){
+            $('#potential').html(select_state_to_see);
         } else {
-            msg = diff_visibility_text;
+            $('#potential').html(this.name);
+            var new_visibility = "hidden";
+            selected_ras.each(function( index ) {
+                var ra = $(this).val();
+                console.log(
+                    status_matrix[ra].states[state]
+                );
+                var potential_vis = status_matrix[ra].states[state];
+                if (potential_vis == "public") {
+                    new_visibility = potential_vis;
+                } else if (new_visibility == "public") {
+                    // Do nothing, if one thinks its public, its public
+                } else if (potential_vis == "locked") {
+                    new_visibility = potential_vis;
+                }
+            });
+            var msg = "";
+            var vis_trans = visibility_translations[new_visibility];
+            if (new_visibility == current_visibility) {
+                msg = same_visibility_text;
+            } else {
+                msg = diff_visibility_text;
+            }
+            msg = msg.replace('VISIBILITY_STATUS',vis_trans);
+
+            $('#potential').html(msg);
         }
-        msg = msg.replace('VISIBILITY_STATUS',vis_trans);
-        
-        $('#potential').html(msg);
-    }
+    });
 });
