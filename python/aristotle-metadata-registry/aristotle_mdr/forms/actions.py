edfc6769da2d70b69ca39f8449c431f606a9fe99
Harry White <harry.white424@gmail.com>
2018-03-21 14:31:40 +1100

    Added clean method for form

--- python/aristotle-metadata-registry/aristotle_mdr/forms/actions.py
+++ python/aristotle-metadata-registry/aristotle_mdr/forms/actions.py
@@ -8,6 +8,8 @@ from aristotle_mdr.widgets.bootstrap import BootstrapDateTimePicker
 from django.contrib.auth import get_user_model
 
 from aristotle_mdr.contrib.autocomplete import widgets
+from aristotle_mdr.perms import can_delete_metadata
+from aristotle_mdr.models import _concept
 from .utils import RegistrationAuthorityMixin
 
 
@@ -95,3 +97,12 @@ class DeleteSandboxForm(UserAwareForm):
             queryset=_concept.objects.filter(submitter=self.user),
             required=True,
         )
+
+    def clean_item(self):
+
+        item = self.cleaned_data['item']
+
+        if not can_delete_metadata(self.user, item):
+            raise ValidationError("Item could not be deleted", code="invalid")
+
+        return item
