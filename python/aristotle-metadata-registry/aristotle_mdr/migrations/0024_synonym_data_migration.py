c71630d786429f56a7caa5fa3af3eb649a7ddc80
Harry White <harry.white424@gmail.com>
2018-04-05 10:57:00 +1000

    Added back reverse with prints for not completed

--- python/aristotle-metadata-registry/aristotle_mdr/migrations/0024_synonym_data_migration.py
+++ python/aristotle-metadata-registry/aristotle_mdr/migrations/0024_synonym_data_migration.py
@@ -13,27 +13,28 @@ def add_slots(apps, schema_editor):
 
         for concept in _concept.objects.all():
             if concept.synonyms:
-                print('added one')
                 slot.objects.create(
                     name='Synonyms',
                     concept=concept,
                     value=concept.synonyms
                 )
+    else:
+        print("Data migration could not be completed")
 
 def reverse_add_slots(apps, schema_editor):
 
-    pass
-    # try:
-    #     slot = apps.get_model('aristotle_mdr_slots', 'Slot')
-    # except LookupError:
-    #     slot = None
-    #
-    # print(slot)
-    # if slot:
-    #     for s in slot.objects.all():
-    #         if s.name == 'Synonyms' and len(s.value) < 200:
-    #             s.concept.synonyms = s.value
-    #             s.concept.save()
+    try:
+        slot = apps.get_model('aristotle_mdr_slots', 'Slot')
+    except LookupError:
+        slot = None
+
+    if slot:
+        for s in slot.objects.all():
+            if s.name == 'Synonyms' and len(s.value) < 200:
+                s.concept.synonyms = s.value
+                s.concept.save()
+    else:
+        print('Reverse data migration could not be completed')
 
 class Migration(migrations.Migration):
 
