9ea29f357b014d7b22544c7dcda27ec9dcc1809e
Harry White <harry.white424@gmail.com>
2018-04-23 16:37:38 +1000

    url change

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/urls.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/urls.py
@@ -3,5 +3,5 @@ from graphene_django.views import GraphQLView
 from aristotle_mdr_graphql.schema.schema import schema
 
 urlpatterns = [
-    url(r'^graphql', GraphQLView.as_view(graphiql=True, schema=schema))
+    url(r'^api', GraphQLView.as_view(graphiql=True, schema=schema), name='graphql_api')
 ]
