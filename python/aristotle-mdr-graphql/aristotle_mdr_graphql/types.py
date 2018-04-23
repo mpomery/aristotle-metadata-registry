7be252420fa1f9806127adccd6cd9ee8b89c3fde
Harry White <harry.white424@gmail.com>
2018-04-23 16:31:33 +1000

    Fix for related set permissions, added comments

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/types.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/types.py
@@ -2,21 +2,46 @@ from graphene_django.types import DjangoObjectType
 from aristotle_mdr import models as mdr_models
 from graphene import relay
 from django.db.models import Model
+from django.db.models.manager import Manager
+from django.db.models.query import Queryset
+
+import logging
+logger = logging.getLogger(__name__)
 
 def aristotle_resolver(attname, default_value, root, info, **args):
     retval = getattr(root, attname, default_value)
 
+    #logger.debug('retval type is {}'.format(type(retval)))
+
+    # If object is a django model
     if isinstance(retval, Model):
 
+        # If model has can_view method
         if hasattr(retval, 'can_view'):
+
+            # Use can_view to determine if we display
             if retval.can_view(info.context.user):
                 return retval
             else:
                 return None
         else:
+
+            # If no can_view method return value
             return retval
 
+    elif isinstance(retval, Manager):
+
+        # Need this for when related manager is returned when querying object.related_set
+        # Can safely return restricted queryset
+        return retval.get_queryset().visible(info.context.user)
+
+    elif isinstance(retval, Queryset):
+
+        # In case a queryset is returned
+        return retval.visible(info.context.user)
+
     return retval
+
 
 class AristotleObjectType(DjangoObjectType):
 
