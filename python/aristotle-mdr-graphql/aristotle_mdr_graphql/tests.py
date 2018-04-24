6cb6ae1d9b0d228f5cd1380ab8559ca110cda4c6
Harry White <harry.white424@gmail.com>
2018-04-24 14:40:17 +1000

    Added permission tests

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/tests.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/tests.py
@@ -4,6 +4,7 @@ from aristotle_mdr.tests import utils
 from aristotle_mdr import models as mdr_models
 
 import json
+import datetime
 
 class BaseGraphqlTestCase(utils.LoggedInViewPages):
 
@@ -74,6 +75,7 @@ class GraphqlPermissionsTests(BaseGraphqlTestCase, TestCase):
         )
 
     def test_query_workgroup_items(self):
+        # Test querying items in the users workgroup
 
         self.login_editor() # Editor is in wg1
         json_response = self.post_query('{ metadata { edges { node { name } } } }')
@@ -86,9 +88,27 @@ class GraphqlPermissionsTests(BaseGraphqlTestCase, TestCase):
         self.assertEqual(edges[0]['node']['dataElementConcept']['name'], 'Test Data Element Concept')
         self.assertEqual(edges[0]['node']['valueDomain']['name'], 'Test Value Domain')
 
+    def test_query_non_workgroup_items(self):
+        # Test querying items not in the users workgroup
+        self.login_regular_user()
+
+        json_response = self.post_query('{ metadata { edges { node { name } } } }')
+        self.assertEqual(len(json_response['data']['metadata']['edges']), 0)
+
+        json_response = self.post_query('{ dataElements { edges { node { name } } } }')
+        self.assertEqual(len(json_response['data']['dataElements']['edges']), 0)
+
+        json_response = self.post_query('{ dataElementConcepts { edges { node { name } } } }')
+        self.assertEqual(len(json_response['data']['dataElementConcepts']['edges']), 0)
+
+        json_response = self.post_query('{ valueDomains { edges { node { name } } } }')
+        self.assertEqual(len(json_response['data']['valueDomains']['edges']), 0)
+
     def test_anon_request_toplevel(self):
+        # Test  querying from top level with anon user
 
         self.client.logout()
+
         self.vd._is_public = True
         self.vd.save()
 
@@ -101,3 +121,63 @@ class GraphqlPermissionsTests(BaseGraphqlTestCase, TestCase):
         json_response = self.post_query('{ valueDomains { edges { node { name } } } }')
         self.assertEqual(len(json_response['data']['valueDomains']['edges']), 1)
         self.assertEqual(json_response['data']['valueDomains']['edges'][0]['node']['name'], 'Test Value Domain')
+
+    def test_query_not_allowed_foreign_key(self):
+        # Test accessing an item user doesnt have permission to view through a foreign key
+
+        self.vd.workgroup = self.wg2
+        self.vd.save()
+
+        self.login_editor()
+
+        json_response = self.post_query('{ dataElements { edges { node { name dataElementConcept { name } valueDomain { name } } } } }')
+        edges = json_response['data']['dataElements']['edges']
+        self.assertEqual(len(edges), 1)
+        self.assertEqual(edges[0]['node']['name'], 'Test Data Element')
+        self.assertEqual(edges[0]['node']['dataElementConcept']['name'], 'Test Data Element Concept')
+        self.assertEqual(edges[0]['node']['valueDomain'], None)
+
+    def test_query_not_allowed_related_set(self):
+        # Test accessing an item user doesnt have permission to view through a related set
+
+        self.de.workgroup = self.wg2
+        self.de.save()
+
+        self.login_editor()
+
+        json_response = self.post_query('{ valueDomains { edges { node { name dataelementSet { edges { node { name } } } } } } }')
+        edges = json_response['data']['valueDomains']['edges']
+        self.assertEqual(len(edges), 1)
+        self.assertEqual(edges[0]['node']['name'], 'Test Value Domain')
+        self.assertEqual(len(edges[0]['node']['dataelementSet']['edges']), 0)
+
+    def test_query_not_allowed_m2m(self):
+        # Test accessing an item user doesnt have permission to view through a many to many relation
+
+        self.vd.workgroup = self.wg2
+        self.vd.save()
+
+        rr = mdr_models.ReviewRequest.objects.create(
+            requester=self.editor,
+            registration_authority=self.ra,
+            status=0,
+            state=0,
+            registration_date=datetime.date.today(),
+            cascade_registration=0
+        )
+
+        rr.concepts.add(self.de)
+        rr.concepts.add(self.dec)
+        rr.concepts.add(self.vd)
+
+        self.login_editor()
+
+        json_response = self.post_query('{ reviewRequests { edges { node { concepts { edges { node { name } } } } } } }')
+        edges = json_response['data']['reviewRequests']['edges']
+        self.assertEqual(len(edges), 1)
+
+        concept_edges = edges[0]['node']['concepts']['edges']
+        self.assertEqual(len(concept_edges), 2)
+
+        for item in concept_edges:
+            self.assertNotEqual(item['node']['name'], 'Test Value Domain')
