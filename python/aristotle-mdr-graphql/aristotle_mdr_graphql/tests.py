6f190d4e68fa42cbd1796bdcc0524eb4762b562c
Harry White <harry.white424@gmail.com>
2018-04-24 13:01:07 +1000

    added post_query util function, initial perms test

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/tests.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/tests.py
@@ -13,10 +13,21 @@ class BaseGraphqlTestCase(utils.LoggedInViewPages):
         self.client = Client()
         self.apiurl = reverse('aristotle_graphql:graphql_api')
 
+    def post_query(self, qstring):
+        postdata = {
+            'query': qstring
+        }
+
+        jsondata = json.dumps(postdata)
+        response = self.client.post(self.apiurl, jsondata, 'application/json')
+        self.assertEqual(response.status_code, 200)
+        response_json = json.loads(response.content)
+        return response_json
+
 @override_settings(SECURE_SSL_REDIRECT=False)
 class GraphqlFunctionalTests(BaseGraphqlTestCase, TestCase):
 
-    @tag('runthis')
+
     def test_get_metadata(self):
         mdr_models.ObjectClass.objects.create(
             name="Test Object Class",
@@ -25,26 +36,53 @@ class GraphqlFunctionalTests(BaseGraphqlTestCase, TestCase):
         )
 
         self.login_editor()
-
-        postdata = {
-            'query': '{ metadata { edges { node { name } } } }'
-        }
-        
-        jsondata = json.dumps(postdata)
-        response = self.client.post(self.apiurl, jsondata, 'application/json')
-        self.assertEqual(response.status_code, 200)
-
-        response_json = json.loads(response.content)
+        response_json = self.post_query('{ metadata { edges { node { name } } } }')
         self.assertEqual(response_json['data']['metadata']['edges'][0]['node']['name'], 'Test Object Class')
 
     def test_load_graphiql(self):
 
         self.login_editor()
 
-        print(self.apiurl)
         response = self.client.get(self.apiurl, HTTP_ACCEPT='text/html')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('graphene/graphiql.html')
 
 @override_settings(SECURE_SSL_REDIRECT=False)
-class GraphqlPermissionsTests(TestCase):
-    pass
+class GraphqlPermissionsTests(BaseGraphqlTestCase, TestCase):
+
+    def setUp(self):
+
+        super().setUp()
+
+        self.dec = mdr_models.DataElementConcept.objects.create(
+            name='Test Data Element Concept',
+            definition='Test Defn',
+            workgroup=self.wg1
+        )
+
+        self.vd = mdr_models.ValueDomain.objects.create(
+            name='Test Value Domain',
+            definition='Test Defn',
+            workgroup=self.wg1
+        )
+
+        self.de = mdr_models.DataElement.objects.create(
+            name='Test Data Element',
+            definition='Test Defn',
+            workgroup=self.wg1,
+            dataElementConcept=self.dec,
+            valueDomain=self.vd
+        )
+
+    def test_query_workgroup_items(self):
+
+        self.login_editor()
+        json_response = self.post_query('{ metadata { edges { node { name } } } }')
+        self.assertEqual(len(json_response['data']['metadata']['edges']), 3)
+
+        json_response = self.post_query('{ dataElements { edges { node { name dataElementConcept { name } valueDomain { name } } } } }')
+        edges = json_response['data']['dataElements']['edges']
+        self.assertEqual(len(edges), 1)
+        self.assertEqual(edges[0]['node']['name'], 'Test Data Element')
+        self.assertEqual(edges[0]['node']['dataElementConcept']['name'], 'Test Data Element Concept')
+        self.assertEqual(edges[0]['node']['valueDomain']['name'], 'Test Value Domain')
