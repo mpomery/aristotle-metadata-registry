bbff7e94cd1a8ca049a2c2473600231acf62ff1b
Harry White <harry.white424@gmail.com>
2018-04-30 10:30:35 +1000

    Removed loggin from util function

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/utils.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/utils.py
@@ -1,12 +1,9 @@
 from aristotle_mdr_graphql.types import AristotleObjectType
-import logging
-logger = logging.getLogger(__name__)
 
 def type_from_model(meta_model):
 
     modelname = meta_model.__name__
     new_modelname = modelname + 'Node'
-    logger.debug(new_modelname)
 
     meta_class = type('Meta', (object, ), dict(model=meta_model))
     dynamic_class = type(new_modelname, (AristotleObjectType, ), dict(Meta=meta_class))
