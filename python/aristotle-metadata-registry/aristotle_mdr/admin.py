bade7bf7b90d55550104a9c74e95a8175c85b949
Harry White <harry.white424@gmail.com>
2018-05-04 14:44:31 +1000

    Added admin inlines

--- python/aristotle-metadata-registry/aristotle_mdr/admin.py
+++ python/aristotle-metadata-registry/aristotle_mdr/admin.py
@@ -405,10 +405,22 @@ class aristotle_mdr_DataElementDerivationSearchIndex(conceptIndex, indexes.Index
             Q(dataelementconcept__dataelement__derived_from=obj) | Q(dataelementconcept__dataelement__input_to_derivation=obj)
         ).values_list('name', flat=True))
 
+class DedDerivesInline(admin.TabularInline):
+
+    model = MDR.DedDerivesThrough
+    verbose_name = "Derive"
+    verbose_name_plural = "Derives"
+
+class DedInputsInline(admin.TabularInline):
+
+    model = MDR.DedInputsThrough
+    verbose_name = "Input"
+    verbose_name_plural = "Inputs"
 
 register_concept(
     MDR.DataElementDerivation,
-    extra_fieldsets=[('Derivation', {'fields': ['derivation_rule', 'derives', 'inputs']})],
+    extra_fieldsets=[('Derivation', {'fields': ['derivation_rule']})],
+    extra_inlines=[DedDerivesInline, DedInputsInline],
     custom_search_index=aristotle_mdr_DataElementDerivationSearchIndex
 )
 
