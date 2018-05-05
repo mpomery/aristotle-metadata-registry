f577da759b762e5204ca8571630ccba672cc610b
EC2 Default User <ec2-user@ip-172-31-76-76.ec2.internal>
2018-05-05 02:19:22 +0000

    alter urls, added github-like explorer

--- python/aristotle-mdr-graphql/aristotle_mdr_graphql/urls.py
+++ python/aristotle-mdr-graphql/aristotle_mdr_graphql/urls.py
@@ -1,7 +1,29 @@
 from django.conf.urls import url
 from graphene_django.views import GraphQLView
 from aristotle_mdr_graphql.schema.schema import schema
+from aristotle_mdr_graphql.views import FancyGraphQLView
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^api', GraphQLView.as_view(graphiql=True, schema=schema), name='graphql_api')
+    url(r'^/?$', TemplateView.as_view(template_name="aristotle_mdr_graphql/explorer.html"), name='graphql_explorer'),
+    url(r'^api', FancyGraphQLView.as_view(
+        graphiql=True,
+        schema=schema,
+        default_query="\n".join([
+            "# This query fetches the name of the first 5 metadata items you"
+            "# have permission to see",
+            "# Use the documentation on the right to build futher queries",
+            "",
+            "query {",
+            "  metadata (first: 5) {",
+            "    edges {",
+            "      node {",
+            "        name",
+            "        definition",
+            "      }",
+            "    }",
+            "  }",
+            "}",
+        ])
+    ), name='graphql_api'),
 ]
