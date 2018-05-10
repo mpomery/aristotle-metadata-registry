ec50b704e86dc8223cae14272d4dfb10f00aabf7
Harry White <harry.white424@gmail.com>
2018-05-10 16:16:54 +1000

    Added unique_together on through tables

--- python/aristotle-metadata-registry/aristotle_mdr/models.py
+++ python/aristotle-metadata-registry/aristotle_mdr/models.py
@@ -1347,6 +1347,7 @@ class DedBaseThrough(models.Model):
     class Meta:
         abstract = True
         ordering = ['order']
+        unique_together = ('data_element_derivation', 'data_element')
 
 
 class DedDerivesThrough(DedBaseThrough):
