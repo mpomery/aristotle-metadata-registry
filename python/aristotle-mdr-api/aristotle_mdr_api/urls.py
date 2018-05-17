24f145e42f62eb5d5c2384f4a30c1778e92b049d
Harry White <harry.white424@gmail.com>
2018-05-17 12:51:23 +1000

    Added basic get token view

--- python/aristotle-mdr-api/aristotle_mdr_api/urls.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from rest_framework.documentation import include_docs_urls
 from rest_framework_swagger.views import get_swagger_view
 from django.utils.module_loading import import_string
-from .views import APIRootView
+from .views import APIRootView, GetTokenView
 from rest_framework.authtoken import views as tokenviews
 
 import re
@@ -32,6 +32,7 @@ def version_schema(*args, **kwargs):
 urlpatterns = [
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', tokenviews.obtain_auth_token),
+    url(r'^gettoken/', GetTokenView.as_view()),
 
     url(r'^(?P<version>(v2|v3)?)/schemas/', version_schema),
     url(r'^schemas/', get_swagger_view(title='Aristotle API')),
--- python/aristotle-mdr-api/aristotle_mdr_api/views.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/views.py
@@ -2,13 +2,21 @@ from django.contrib.auth.decorators import permission_required
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from rest_framework.authtoken.models import Token
 
 
 class APIRootView(TemplateView):
     template_name = "aristotle_mdr_api/base.html"
 
 
-class GetTokenView(TemplateView):
-    template_name = "aristotle_mdr_api/api_message.html"
+class GetTokenView(LoginRequiredMixin, TemplateView):
+    template_name = "aristotle_mdr_api/token.html"
 
-    # def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
+
+        token, created = Token.objects.get_or_create(user=request.user)
+        kwargs['token'] = token.key
+
+        return super().get(request, *args, **kwargs)
