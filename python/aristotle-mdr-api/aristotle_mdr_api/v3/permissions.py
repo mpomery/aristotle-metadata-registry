e5ef0d389e4e3d2d6935ad5ef77bc2a2e274921c
Harry White <harry.white424@gmail.com>
2018-05-17 15:00:41 +1000

    Initial token permission

--- python/aristotle-mdr-api/aristotle_mdr_api/v3/permissions.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/v3/permissions.py
@@ -14,3 +14,45 @@ class IsSuperuserOrReadOnly(permissions.BasePermission):
             request.user.is_authenticated() and
             request.user.is_superuser
         )
+
+
+class TokenPermissions(permissions.BasePermission):
+
+    permission_key = 'default'
+
+    def is_authenticated(self, request):
+        return request.user and request.user.is_authenticated
+
+    def has_permission(self, request, view):
+
+        authenticated = self.is_authenticated(request)
+
+        if not authenticated:
+            return False
+
+        if hasattr(view, 'permission_key'):
+            self.permission_key = getattr(view, 'permission_key')
+
+        token = request.auth
+
+        # request.auth will be None when using any other default authentication class
+        if token is not None:
+            perms = token.permissions
+
+            if self.permission_key in perms.keys():
+                permtext = perms[self.permission_key]
+                hasread = 'r' in permtext
+                haswrite = 'w' in permtext
+
+        else:
+            # Default for non token auths
+            hasread = True
+            haswrite = True
+
+        if request.method in permissions.SAFE_METHODS and hasread:
+            return True
+
+        if request.method not in permissions.SAFE_METHODS and haswrite:
+            return True
+
+        return False
