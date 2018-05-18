c2982a25d83f2dc83e39257c572b7df3ef4bcf32
Harry White <harry.white424@gmail.com>
2018-05-18 11:49:18 +1000

    token create form schema

--- python/aristotle-mdr-api/aristotle_mdr_api/forms.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/forms.py
@@ -1,9 +1,18 @@
 from django.forms import ModelForm, Form
+from django.forms.fields import CharField
 from django_jsonforms.forms import JSONSchemaField
 
 class TokenCreateForm(Form):
 
-    json = JSONSchemaField(
+    name = CharField()
+    perm_json = JSONSchemaField(
         schema = 'aristotle_mdr_api/schema.json',
-        options = {'theme': 'bootstrap3'}
+        options = {
+            'theme': 'bootstrap3',
+            'disable_properties': True,
+            'disable_collapse': True,
+            'disable_edit_json': True,
+            'no_additional_properties': True
+        },
+        label=''
     )
