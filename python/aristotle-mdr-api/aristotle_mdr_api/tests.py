cd9060a0ed7ea040c243c3e9d8ce38837e5fd084
Harry White <harry.white424@gmail.com>
2018-05-18 15:51:53 +1000

    First token perms test

--- python/aristotle-mdr-api/aristotle_mdr_api/tests.py
+++ python/aristotle-mdr-api/aristotle_mdr_api/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase, Client, override_settings
+from django.test import TestCase, Client, override_settings, tag
 from django.urls import reverse
 
 from aristotle_mdr.tests import utils
+from aristotle_mdr_api.models import AristotleToken
+
+from rest_framework.test import APIClient
 
 import json
 
@@ -11,6 +14,7 @@ class TokenTestCase(utils.LoggedInViewPages, TestCase):
     def setUp(self):
         super().setUp()
         self.client = Client()
+        self.apiclient = APIClient()
 
     def post_token_create(self, name, perms):
 
@@ -32,6 +36,7 @@ class TokenTestCase(utils.LoggedInViewPages, TestCase):
 
         self.login_viewer()
 
+        self.assertEqual(AristotleToken.objects.count(), 0)
         response = self.client.get(reverse('token_create'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'aristotle_mdr_api/token_create.html')
@@ -45,7 +50,7 @@ class TokenTestCase(utils.LoggedInViewPages, TestCase):
                 'read': True,
                 'write': False
             },
-            'workgroup': {
+            'organization': {
                 'read': True,
                 'write': False
             },
@@ -56,3 +61,48 @@ class TokenTestCase(utils.LoggedInViewPages, TestCase):
         }
 
         token = self.get_token('MyToken', perms)
+        self.assertEqual(AristotleToken.objects.count(), 1)
+
+    def test_token_perms(self):
+
+        self.login_viewer()
+
+        perms = {
+            'metadata': {
+                'read': True,
+                'write': False
+            },
+            'search': {
+                'read': False,
+                'write': False
+            },
+            'organization': {
+                'read': False,
+                'write': False
+            },
+            'ra': {
+                'read': True,
+                'write': False
+            }
+        }
+
+        token = self.get_token('MyToken', perms)
+
+        self.client.logout()
+
+        auth = 'Token {}'.format(token)
+
+        response = self.client.get('/api/v3/metadata/', HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/api/v3/search/', HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.get('/api/v3/organizations/', HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.get('/api/v3/ras/', HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/api/v3/types/', HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, 200)
