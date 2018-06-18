a23dde7ed8ea419980127ea20851c2ef74aaf0d0
Harry White <harry.white424@gmail.com>
2018-06-18 15:02:54 +1000

    working config for aristotle_mdr

---
+++ python/aristotle-metadata-registry/manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+import sys
+print(sys.version)
+
+if __name__ == "__main__":
+    if 'test' in sys.argv:
+        os.environ['DJANGO_SETTINGS_MODULE'] = "aristotle_mdr.tests.settings.settings"
+    elif 'schemamigration' in sys.argv:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aristotle_mdr.tests.settings.settings")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aristotle_mdr.required_settings")
+
+    from django.core.management import execute_from_command_line
+
+execute_from_command_line(sys.argv)
