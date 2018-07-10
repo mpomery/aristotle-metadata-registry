d623fa167d5572ae0b42e77a0a3af0ccf6625c42
Sam Spencer <theodore.therone@gmail.com>
2018-07-10 01:28:54 +0000

    change to always check on master, remove cruft in travis to reduce output

--- scripts/tox_if.sh
+++ scripts/tox_if.sh
@@ -2,7 +2,7 @@
 
 TOX_ENV_TO_RUN=$1
 
-if ./scripts/check_path.sh $MODULE_PATH || ./scripts/check_path.sh python/aristotle-metadata-registry; then
+if [ "$TRAVIS_BRANCH" = "master" ] || ./scripts/check_path.sh $MODULE_PATH || ./scripts/check_path.sh python/aristotle-metadata-registry; then
   tox -e $TOX_ENV_TO_RUN --skip-missing-interpreters
   export OUT_CODE=$?
 fi;
